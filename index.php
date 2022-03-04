26d1f56465de0bb6775bf4802a0da8c9846f9da6
Levi Sutton <levi.sutton2013@gmail.com>
2022-03-03 20:37:25 -0500

    Updating portfolio with projects

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    $temp_time = microtime()
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Levi Sutton</title>
 
-    <link rel="stylesheet" href="css/portfolio.css">
+    <link rel="stylesheet" href=<?php echo '"css/portfolio.css?v'.$temp_time.'"'?>>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
--- js/project.js
+++ js/project.js
@@ -9,8 +9,6 @@ class Project extends React.Component{
     }
 
     render(){
-
-        console.log(this.state.selectedProject)
         
         return(
             <div className="portfolio-container">
@@ -23,11 +21,112 @@ class Project extends React.Component{
 
                     <div className="portfolio-container-body">
                         <div className="portfolio-container-about-cotainer-wrapper">
-                            <div className="portfolio-container-project-header-wrapper">
-                                <div className="portfolio-container-project-header">
-                                    <span>{this.state.selectedProject.title}</span>
-                                </div>
+                            <div className="portfolio-container-project-header">
+                                <span>{this.state.selectedProject.title}</span>
+                                {
+                                    this.state.selectedProject.github == true || this.state.selectedProject.website == true ? 
+                                        <div className="portfolio-container-project-links-container">
+                                            {
+                                                this.state.selectedProject.github == true ? 
+                                                    <a className="contact-links" href={this.state.selectedProject.github_link} target="_blank">
+                                                        <i className="fab fa-github"></i>
+                                                    </a>
+                                                : ""
+                                            }      
+                                            {
+                                                this.state.selectedProject.website == true ? 
+                                                    <a className="contact-links" href={this.state.selectedProject.website_link} target="_blank">
+                                                        <i className="fab fa-github"></i>
+                                                    </a>
+                                                : ""
+                                            }                                         
+                                        </div>
+                                    : ""
+                                }
                             </div>
+                            {
+                                this.state.selectedProject.project_description != null ? 
+                                    <div className="portfolio-container-project-description-container-wrapper">
+                                        <div className="portfolio-container-project-description-container-header">
+                                            <span>Project Description</span>
+                                        </div>
+                                        <div className="portfolio-container-project-description-container">
+                                            <p>{this.state.selectedProject.project_description}</p>
+                                        </div>
+                                    </div>
+                                : ""
+                            }
+
+                            {
+                                this.state.selectedProject.problem != null ? 
+                                    <div className="portfolio-container-project-description-container-wrapper">
+                                        <div className="portfolio-container-project-description-container-header">
+                                            <span>Problem</span>
+                                        </div>
+                                        <div className="portfolio-container-project-description-container">
+                                            <p>{this.state.selectedProject.problem}</p>
+                                        </div>
+                                    </div>
+                                : ""
+                            }
+
+{
+                            this.state.selectedProject.features != null ? 
+                                <div className="portfolio-container-project-description-container-wrapper">
+                                    <div className="portfolio-container-project-description-container-header">
+                                        <span>Features</span>
+                                    </div>
+                                    <div className="portfolio-container-project-description-container">
+                                        <ul>
+                                            {
+                                                this.state.selectedProject.features.map((feature, key) => (
+                                                    <li key={key}>{feature}</li>
+                                                ))
+                                            }
+                                        </ul>
+                                        
+                                    </div>
+                                </div>
+                                : ""
+                            }
+
+                            {
+                                this.state.selectedProject.tools != null ? 
+                                    <div className="portfolio-container-project-description-container-wrapper">
+                                        <div className="portfolio-container-project-description-container-header">
+                                            <span>Tools Used</span>
+                                        </div>
+                                        <div className="portfolio-container-project-description-container">
+                                            <div className="portfolio-container-project-description-tools-container-wrapper">
+                                                {
+                                                    this.state.selectedProject.tools.map((tool, key) => (
+                                                        <div className="portfolio-container-project-description-tools-container"  key={key}>
+                                                            <span>{tool}</span>
+                                                        </div>
+                                                    ))
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                : ""
+                            }
+
+                            {
+                                this.state.selectedProject.project_imgs != null ? 
+                                    <div className="portfolio-container-project-description-container-wrapper">
+                                        <div className="portfolio-container-project-description-container">
+                                            <img src=""/>
+                                        </div>
+                                    </div>
+                                : 
+
+                                <div className="portfolio-container-project-description-container-wrapper">
+                                    <div className="portfolio-container-project-description-container">
+                                        <span><span className="error-message">*Images not shown due to company policy*. </span><br/><br/>  Want to learn more about this project? <a href="#" href="mailto: levi.sutton2013@gmail.com" target="_blank">Lets chat</a>.</span>
+                                    </div>
+                                </div>
+                            }
+                            
                         </div>
                     </div>
                 </div>
